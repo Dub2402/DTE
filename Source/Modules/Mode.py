a76c6b6d3c268f738f5832ca40dc8dddf2bcb6fd
Irina Dub <109979502+Dub2402@users.noreply.github.com>
2025-09-22 21:27:27 +0300

    Update Mode.py

--- Source/Modules/Mode.py
+++ Source/Modules/Mode.py
@@ -34,12 +34,38 @@ class InlineTemplates:
 			_("👼 Няшка"): "sweetie",
 			_("🍺 Кореш"): "buddy",
 			_("💪 Мотиватор"): "motivator",
-			_("🦖 Газлайтер (18+)"): "gaslighter",
+			_("🦖 Газлайтер (18+)"): "approve_18",
 			_("🚦 Рандом"): "random",
 			_("🔙 Back"): "delete_mode",
 		}
 
 		for string in modes.keys(): menu.add(types.InlineKeyboardButton(string, callback_data = modes[string]), row_width = 1)
+
+		return menu
+	
+	def use_mode():
+
+		menu = types.InlineKeyboardMarkup()
+
+		modes = {
+			_("Применить"): "2",
+			_("🔙 Назад"): "3"
+		}
+
+		for string in modes.keys(): menu.add(types.InlineKeyboardButton(string, callback_data = modes[string]), row_width = 1)
+
+		return menu
+	
+	def is_18():
+
+		menu = types.InlineKeyboardMarkup()
+
+		modes = {
+			_("Да"): "gaslighter",
+			_("Нет"): "1"
+		}
+
+		for string in modes.keys(): menu.add(types.InlineKeyboardButton(string, callback_data = modes[string]), row_width = 2)
 
 		return menu
 
@@ -65,25 +91,62 @@ class Decorators:
 			
 			self.__modes.bot.answer_callback_query(Call.id)
 
+		@self.__modes.bot.callback_query_handler(func = lambda Callback: Callback.data == "approve_18")
+		def choice_bot_mode(Call: types.CallbackQuery):
+			user = self.__modes.users.auth(Call.from_user)
+
+			text_approval = (
+				"<b>" + _("Осторожно! ") + "</b>" + _("Следующий материал несёт в себе нецензурные и оскорбительные высказывания. Он предназначен строго для лиц от 18 лет!" + "\n"),
+				"<b>" + _("Уверены в том, что хотите просмотреть содержимое? Вам есть 18?") + "</b>"
+				)
+
+			self.__modes.bot.send_message(
+				chat_id = Call.message.chat.id,
+				text = "\n".join(text_approval),
+				parse_mode = "HTML",
+				reply_markup = InlineTemplates.is_18()
+				)
+			
+			self.__modes.bot.answer_callback_query(Call.id)
+
 		@self.__modes.bot.callback_query_handler(func = lambda Callback: Callback.data in ("classic", "sweetie", "buddy", "motivator", "gaslighter", "random"))
 		def choice_bot_mode(Call: types.CallbackQuery):
 			user = self.__modes.users.auth(Call.from_user)
 
+			mode = Call.data
+
 			texts = {
-			"classic": _("Классик - это режим бота, который общается с вами в формально-официальном тоне. Все чётко, без лишних слов и по существу. Пример: День рождения наступит через 41 день!"),
-			"sweetie": _("👼 Няшка"),
-			"buddy": _("🍺 Кореш"),
-			"motivator": _("💪 Мотиватор"),
-			"gaslighter": _("🦖 Газлайтер (18+)"),
-			"random": _("🚦 Рандом")
-		}
-
-
+			"classic": (
+				"<b>" + _("Классик ") + "</b>" + _("- это режим бота, который общается с вами в формально-официальном тоне. Все чётко, без лишних слов и по существу. ") + "<i>" +  "Пример:" + "</i>\n",
+				"<b>" + _("День рождения ") + "</b>" + _("наступит через") + "<b>" +  " 41 " + "</b>" + _("день!")
+				),
+			"sweetie": (
+				"<b>" + _("Няшка ") + "</b>" + _("- это режим бота, который общается с вами ласково и нежно. Его задача вызвать у вас теплые эмоции и расположить. ") + "<i>" +  "Пример:" + "</i>\n",
+				_("Солнышко моё! ") + "<b>" + _("День рождения ") + "</b>" + _("наступит через") + "<b>" +  " 41 " + "</b>" + _("день! Главное не забывай улыбаться! ☺️")
+				),
+			"buddy": (
+				"<b>" + _("Кореш ") + "</b>" + _("- это режим бота, который имитирует вашего давнего приятеля, с которым вы хорошо знакомы. Он общается довольно фамильярно. ") + "<i>" +  "Пример:" + "</i>\n",
+				_("Братуухаа! ") + "<b>" + _("День рождения ") + "</b>" + _("наступит через") + "<b>" +  " 41 " + "</b>" + _("день! Давай там, хвост пистолетом, обнял!")
+				),
+			"motivator": (
+				"<b>" + _("Мотиватор ") + "</b>" + _("- это режим бота, который старается зарядить вас позитивной энергией, взбодрить и вызвать прилив сил. ") + "<i>" +  "Пример:" + "</i>\n",
+				_("Эй, кто тут самый крутой?) ") + "<b>" + _("День рождения ") + "</b>" + _("наступит через") + "<b>" +  " 41 " + "</b>" + _("день! А пока, хватай удачу за хвост!")
+				),
+			"gaslighter": (
+				"<b>" + _("Газлайтер ") + "</b>" + _("- это режим бота, который будет оскорблять вас, всячески унижать, в общем относится, как к полному ничтожеству. ") + "<i>" +  "Пример:" + "</i>\n",
+				_("Слышишь, у#бище! ") + "<b>" + _("День рождения ") + "</b>" + _("наступит через") + "<b>" +  " 41 " + "</b>" + _("день! Живи, пока я тебе п#зды не дал!")
+				),
+			"random": (
+				"<b>" + _("Рандом ") + "</b>" + _("- это режим бота, который совмещает в себе все образы, представленные выше, и чередует их на свое усмотрение. ") + "\n",
+				_("Сегодня вам может написать Няшка, завтра - Мотиватор, а послезавтра пожалует и сам Газлайтер.")
+				),
+			}
+			
 			self.__modes.bot.send_message(
 				chat_id = Call.message.chat.id,
-				text = "1",
+				text = "\n".join(texts[mode]),
 				parse_mode = "HTML",
-				reply_markup = InlineTemplates.modes_bot()
+				reply_markup = InlineTemplates.use_mode()
 				)
 			
 			self.__modes.bot.answer_callback_query(Call.id)
